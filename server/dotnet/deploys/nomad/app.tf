b7f5b7f383e05859fb1e2a8e505e48392536bade
huynhdev24 <huynh.dev.24@gmail.com>
2024-03-23 11:57:02 +0700

    core: remove nomad

--- server/dotnet/deploys/nomad/app.tf
+++
@@ -1,3 +0,0 @@
-resource "nomad_job" "api" {
-	jobspec = file("${path.module}/jobs/api.nomad.hcl")
-}
--- server/dotnet/deploys/scripts/clean.ps1
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env pwsh
-docker rmi $(docker images -a -q)
-docker rm $(docker ps -a -q)
