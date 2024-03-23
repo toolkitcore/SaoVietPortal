32fa2484bdb3ac59ba7d663ad9fc8ef7ecb64ed8
huynhdev24 <huynh.dev.24@gmail.com>
2024-03-23 11:37:58 +0700

    core: remove client

--- client/application/src/MainPage.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SaoVietPortal;
-
-public partial class MainPage : ContentPage
-{
-    public MainPage()
-    {
-        InitializeComponent();
-    }
-}
--- client/application/src/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace SaoVietPortal;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
